068c1bee525b1d4d8de6ed1d9b5c503eb1ce83f2
Rinaldo <nikilson30@gmail.com>
2021-11-18 09:53:50 +0530

    this is the first commit

---
+++ Git-Gamer.py
@@ -0,0 +1,46 @@
+import os
+from os import path
+from datetime import datetime
+
+# Checking the default location
+# add remote location
+
+home = (path.expanduser("~"))
+git_gamer_location = path.join(home, "GitGamer")
+current_cwd = os.getcwd()
+if not (path.isdir(git_gamer_location)):
+	os.mkdir(git_gamer_location)
+	os.chdir(git_gamer_location)
+	os.system('git init -b "main"')
+	os.chdir(current_cwd)
+	with open("git-repo-url.txt", 'r') as url_file:
+		remote_url = url_file.readline()
+		os.chdir(git_gamer_location)
+		os.system(f"git remote add origin {remote_url}")
+		os.chdir(current_cwd)
+		url_file.close()
+else:
+	pass
+# read location file
+save_location_file = open("save-location.txt", 'r')
+
+game_name_file = open("game-name.txt", 'r')
+
+save_location_list = save_location_file.readlines()
+
+game_name_list = game_name_file.readlines()
+
+for num1 in range(len(save_location_list)):
+	temp_loc = save_location_list[num1][:-1]
+	temp_name = game_name_list[num1][:-1]
+	temp_path = path.join(git_gamer_location, temp_name)
+	if not (path.isdir(temp_path)):
+		os.mkdir(temp_path)
+	temp_loc = temp_loc + "\*"
+	print(f'"{temp_loc}"', f'"{temp_path}"')
+	os.system(f'copy "{temp_loc}" "{temp_path}"')
+os.chdir(git_gamer_location)
+os.system("git add *")
+date_time = datetime.now().strftime("%D%M%Y%H%M%S")
+os.system(f"git commit -m '{date_time}'")
+os.system("git push -u origin main")
